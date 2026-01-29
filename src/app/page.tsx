179b70fae04631ce88f95df9e183197c02bda2fe
25LP1764 25LP1764 <25LP1764@G302-CC0118.local>
2026-01-29 16:04:38 +0800

    home heseg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
 "use client";
 
-const Home = () => {
+import Page from "./HomePage/page";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+export default function HomeMenu() {
+  const router = useRouter();
+  const handleMoreButton = () => {
+    router.push("/HomePage/single_player");
+  };
   return (
-    <div>
-      catch me if u can !!1
-      <p>Try to find me!</p>
-    </div>
-  );
-};
+    <main className="relative min-h-screen overflow-hidden">
+      {/* Background */}
+      <Page />
 
-export default Home;
+      {/* Foreground content */}
+      <div className="relative center flexrelative z-10 min-h-screen flex flex-col items-center justify-start pt-124 gap-4.75 pr-40">
+        <button
+          className="transition active:translate-y-1"
+          onClick={handleMoreButton}
+        >
+          <Image
+            src="/PLAY BIUTTON2.png"
+            alt="hello"
+            width={440}
+            height={108}
+          />
+        </button>
+        <button className="transition active:translate-y-1">
+          <Image
+            src="/PLAY BIUTTON3.png"
+            alt="hello"
+            width={440}
+            height={108}
+          />
+        </button>
+        <button className="transition active:translate-y-1">
+          <Image
+            src="/PLAY BIUTTON4.png"
+            alt="hello"
+            width={440}
+            height={108}
+          />
+        </button>
+      </div>
+    </main>
+  );
+}
