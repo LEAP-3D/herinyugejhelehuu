dcdfc970e9b933f27b514a36309af7345137b9f1
ankhaa3334-tech <ankhaa3334@email.com>
2026-02-28 11:41:38 +0800

    sdadf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,9 @@
-"use client";
-import { useRouter } from "next/navigation";
-import Page from "./Home-page/page";
+import HomePage from "./Home-page/page";
 
 export default function HomeMenu() {
-  const router = useRouter();
-  const handleMoreButton = () => {
-    router.push("/Home-page/Player");
-  };
   return (
     <main className="relative min-h-screen overflow-hidden">
-      {/* Background */}
-      <Page />
-      <button
-        className="transition active:translate-y-1"
-        onClick={handleMoreButton}
-      ></button>
+      <HomePage />
     </main>
   );
 }
